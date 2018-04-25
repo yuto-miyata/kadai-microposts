c172fa26f9d75c6ac15e66de8b09aa140e90004d
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-04-25 04:39:21 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -11,7 +11,23 @@ class MicropostsController extends Controller
 {
     public function index()
     {
-        $data = [];
+         $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+            $data += $this->counts($user);
+            return view('users.show', $data);
+        }else {
+            return view('welcome');
+        }
+        return view('welcome', $data);
+        
+        /*$data = [];
         if (\Auth::check()){
             $user = \Auth::user();
             $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
@@ -24,7 +40,7 @@ class MicropostsController extends Controller
             return view('users.show', $data);
         }else {
             return view('welcome');
-        }
+        }*/
     }
     
     public function store(Request $request)
--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
     }
     
     public function follow($userId)
-{
+    {
     // 既にフォローしているかの確認
     $exist = $this->is_following($userId);
     // 自分自身ではないかの確認
@@ -55,11 +55,11 @@ class User extends Authenticatable
         // 未フォローであればフォローする
         $this->followings()->attach($userId);
         return true;
+        }
     }
-}
 
-public function unfollow($userId)
-{
+    public function unfollow($userId)
+    {
     // 既にフォローしているかの確認
     $exist = $this->is_following($userId);
     // 自分自身ではないかの確認
@@ -72,10 +72,18 @@ public function unfollow($userId)
     } else {
         // 未フォローであれば何もしない
         return false;
+        }
     }
-}
 
-public function is_following($userId) {
+    public function is_following($userId) {
     return $this->followings()->where('follow_id', $userId)->exists();
-}
+    }
+
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
+
 }
